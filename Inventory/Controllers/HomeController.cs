2ae093a92c478c3025cae5d56c0e2c272d5fcb6d
agatakolohe <agatakolohe@gmail.com>
2021-01-15 09:18:35 -0800

    create home controller and view

--- Inventory/Controllers/HomeController.cs
+++ Inventory/Controllers/HomeController.cs
@@ -1,16 +1,29 @@
-// using Microsoft.AspNetCore.Mvc;
-// using Inventory.Models;
-// using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Inventory.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
-// namespace Inventory.Controllers
-// {
-//     public class HomeController : Controller
-//     {
+namespace Inventory.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly InventoryContext _db;
+        public HomeController(InventoryContext db)
+        {
+            _db = db;
+        }
 
-//         [HttpGet("/")]
-//         public ActionResult Index()
-//         {
-//             return View();
-//         }
-//     }
-// }
+        [HttpGet("/")]
+        public ActionResult Index()
+        {
+            Dictionary<object, object> model = new Dictionary<object, object>();
+            List<Treat> treats = _db.Treats.ToList();
+            List<Flavor> flavors = _db.Flavors.ToList();
+            model.Add("treats", treats);
+            model.Add("flavors", flavors);
+            return View(model);
+        }
+    }
+}
